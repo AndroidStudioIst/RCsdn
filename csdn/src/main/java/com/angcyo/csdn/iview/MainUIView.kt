b1bd51833d269f44a09f3a2024796ce1039a9b6b
angcyo <angcyo@126.com>
2018-04-06 15:24:59 +0800

    fix slider menu

--- csdn/src/main/java/com/angcyo/csdn/iview/MainUIView.kt
+++ csdn/src/main/java/com/angcyo/csdn/iview/MainUIView.kt
@@ -1,8 +1,12 @@
 package com.angcyo.csdn.iview
 
+import android.view.LayoutInflater
+import android.view.MotionEvent
 import com.angcyo.csdn.R
 import com.angcyo.uiview.base.PageBean
 import com.angcyo.uiview.base.UINavigationView
+import com.angcyo.uiview.kotlin.isDown
+import com.angcyo.uiview.viewgroup.SliderMenuLayout
 
 /**
  * Copyright (C) 2016,深圳市红鸟网络科技股份有限公司 All rights reserved.
@@ -20,5 +24,29 @@ class MainUIView : UINavigationView() {
         pages.add(PageBean(CommendUIView(), "首页", R.drawable.ico_home_n, R.drawable.ico_home_p))
         pages.add(PageBean(RankListUIView(), "排行榜", R.drawable.ico_rank_n, R.drawable.ico_rank_p))
         pages.add(PageBean(ExpertUIView(), "专栏", R.drawable.ico_expert_n, R.drawable.ico_expert_p))
+    }
+
+    override fun haveSliderMenu(): Boolean {
+        return true
+    }
+
+    override fun initSliderMenu(sliderMenuLayout: SliderMenuLayout, inflater: LayoutInflater) {
+        super.initSliderMenu(sliderMenuLayout, inflater)
+        inflater.inflate(R.layout.menu_blog_list_layout, sliderMenuLayout)
+        sliderMenuLayout.sliderCallback = object : SliderMenuLayout.SimpleSliderCallback() {
+            override fun canSlider(menuLayout: SliderMenuLayout, event: MotionEvent): Boolean {
+                if (event.isDown() && event.x > 30 * density()) {
+                    return false
+                }
+                return true
+            }
+        }
+    }
+
+    override fun onViewLoad() {
+        super.onViewLoad()
+        click(R.id.close_button) {
+            sliderMenuLayout?.closeMenu()
+        }
     }
 }
